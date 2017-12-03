ee0f9ccdad4e2f89a900d36561152ee80081e6b4
AdrienMerlier <adrien.merlier@insa-lyon.fr>
2017-12-03 15:37:31 +0100

    Middleware to protect the api, token is now needed and working

--- routes.js
+++ routes.js
@@ -1,11 +1,11 @@
 module.exports = function(app){
 
-	require('./routes/user')(app);
-
-
 
     app.get('/', function(req, res) {
     	res.send('Hello! The API is at http://localhost:3000/');
 	});
+
+	require('./routes/user')(app);
+
 
 }
